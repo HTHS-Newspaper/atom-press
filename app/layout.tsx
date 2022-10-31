5bc80dd9611720efceb409b691a447f80ea985b0
Anish Pallati <anishp0828@gmail.com>
2022-10-31 13:11:59 -0400

    feat: Add meta tags to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,14 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<head>
-				<title>Next.js</title>
+				<meta charSet="UTF-8" />
+				<meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+				<meta
+					name="viewport"
+					content="width=device-width, initial-scale=1.0"
+				/>
+
+				<title>The Atom Press</title>
 			</head>
 			<body>{children}</body>
 		</html>
